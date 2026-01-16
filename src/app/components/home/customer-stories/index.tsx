7f00f25f3b78b9aaa4d92432f7dcb19ac6029a29
Thuve Rakan <123587624+0xthuve@users.noreply.github.com>
2026-01-17 01:02:07 +0530

    refactor: remove motion effects from customer stories component

--- src/app/components/home/customer-stories/index.tsx
+++ src/app/components/home/customer-stories/index.tsx
@@ -1,21 +1,8 @@
 "use client"
 import Image from 'next/image'
 import { TextGenerateEffect } from '@/app/components/ui/text-generate-effect'
-import { motion, useInView } from 'motion/react'
-import { useRef } from 'react'
 
 function CustomerStories() {
-  // Refs for each box
-  const topLeftRef = useRef(null)
-  const topRightRef = useRef(null)
-  const bottomLeftRef = useRef(null)
-  const bottomRightRef = useRef(null)
-
-  // Detect if each box is in view
-  const topLeftInView = useInView(topLeftRef, { once: true })
-  const topRightInView = useInView(topRightRef, { once: true })
-  const bottomLeftInView = useInView(bottomLeftRef, { once: true })
-  const bottomRightInView = useInView(bottomRightRef, { once: true })
 
   return (
     <section>
@@ -36,11 +23,7 @@ function CustomerStories() {
             <div className="flex flex-col gap-6">
               <div className="flex flex-col xl:flex xl:flex-row gap-6">
                 {/* Top Left Box */}
-                <motion.div
-                  ref={topLeftRef}
-                  initial={{ x: -100, y: -100, opacity: 0 }}
-                  animate={topLeftInView ? { x: 0, y: 0, opacity: 1 } : {}}
-                  transition={{ duration: 0.8 }}
+                <div
                   className="p-8 gap-64 rounded-2xl flex flex-col relative bg-[url('/images/home/customerStories/customer_bg_img.jpg')] object-cover bg-center h-full w-full bg-cover bg-no-repeat before:absolute before:inset-0 before:bg-black/50 before:rounded-2xl before:pointer-events-none"
                 >
                   <span className="text-white/60 uppercase text-sm font-medium relative z-10">
@@ -57,14 +40,10 @@ function CustomerStories() {
                       </p>
                     </div>
                   </div>
-                </motion.div>
+                </div>
 
                 {/* Top Right Box */}
-                <motion.div
-                  ref={topRightRef}
-                  initial={{ x: 100, y: -100, opacity: 0 }}
-                  animate={topRightInView ? { x: 0, y: 0, opacity: 1 } : {}}
-                  transition={{ duration: 0.8 }}
+                <div
                   className="flex flex-col justify-between gap-36 xl:max-w-25 bg-pale-yellow rounded-2xl p-8"
                 >
                   <div>
@@ -78,16 +57,12 @@ function CustomerStories() {
                       Clients recommend our design services.
                     </h3>
                   </div>
-                </motion.div>
+                </div>
               </div>
 
               <div className="flex flex-col xl:flex xl:flex-row gap-6">
                 {/* Bottom Left Box */}
-                <motion.div
-                  ref={bottomLeftRef}
-                  initial={{ x: -100, y: 100, opacity: 0 }}
-                  animate={bottomLeftInView ? { x: 0, y: 0, opacity: 1 } : {}}
-                  transition={{ duration: 0.8 }}
+                <div
                   className="flex flex-col justify-between bg-dark_black xl:max-w-25 dark:bg-white/10 rounded-2xl p-8"
                 >
                   <div className="flex flex-col gap-6">
@@ -107,14 +82,10 @@ function CustomerStories() {
                       />
                     </div>
                   </div>
-                </motion.div>
+                </div>
 
                 {/* Bottom Right Box */}
-                <motion.div
-                  ref={bottomRightRef}
-                  initial={{ x: 100, y: 100, opacity: 0 }}
-                  animate={bottomRightInView ? { x: 0, y: 0, opacity: 1 } : {}}
-                  transition={{ duration: 0.8 }}
+                <div
                   className="flex flex-col gap-24 justify-between bg-dark_black/5 dark:bg-white/5 p-8 rounded-2xl"
                 >
                   <div className="flex flex-col gap-6">
@@ -131,7 +102,7 @@ function CustomerStories() {
                       Founder of Chipsland
                     </p>
                   </div>
-                </motion.div>
+                </div>
               </div>
             </div>
           </div>
